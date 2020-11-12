e0d372b9b345725d4afe28c13df27cf28a9bcc42
whuera <whuera@gmail.com>
2020-11-12 16:37:48 -0500

    whuera: new changes in post home

--- target/classes/sql/import-h2.sql
+++ target/classes/sql/import-h2.sql
@@ -5,12 +5,12 @@ ALTER TABLE POST
 -- password in plaintext: "password"
 INSERT INTO USER (user_id, password, email, username, name, last_name, active)
 VALUES
-  (1, '$2a$06$OAPObzhRdRXBCbk7Hj/ot.jY3zPwR8n7/mfLtKIgTzdJa4.6TwsIm', 'user@mail.com', 'user', 'Name', 'Surname',
+  (1, '$2a$06$OAPObzhRdRXBCbk7Hj/ot.jY3zPwR8n7/mfLtKIgTzdJa4.6TwsIm', 'whuera@gmail.com', 'whuera', 'William', 'Heera',
    1);
 -- password in plaintext: "password"
 INSERT INTO USER (user_id, password, email, username, name, last_name, active)
 VALUES
-  (2, '$2a$06$OAPObzhRdRXBCbk7Hj/ot.jY3zPwR8n7/mfLtKIgTzdJa4.6TwsIm', 'johndoe@gmail.com', 'johndoe', 'John', 'Doe', 1);
+  (2, '$2a$06$OAPObzhRdRXBCbk7Hj/ot.jY3zPwR8n7/mfLtKIgTzdJa4.6TwsIm', 'eduardohuera@gmail.com', 'ehuera', 'Eduardo', 'Huera', 1);
 -- password in plaintext: "password"
 INSERT INTO USER (user_id, password, email, username, name, last_name, active)
 VALUES (3, '$2a$06$OAPObzhRdRXBCbk7Hj/ot.jY3zPwR8n7/mfLtKIgTzdJa4.6TwsIm', 'ana@mail.com', 'ana', 'Ana', 'Surname', 1);
@@ -34,77 +34,77 @@ VALUES (3, 2);
 -- Posts
 INSERT INTO POST (post_id, user_id, title, body, create_date)
 VALUES (1, 1, 'Title 1',
-        '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+        'Este es un Blog de prueba, los datos son demostrativos solo para uso educativo o referencia para quien programo este blog',
         --         CURRENT_TIMESTAMP());
         {ts '2016-10-19 11:10:13.247'});
 INSERT INTO POST (post_id, user_id, title, body, create_date)
 VALUES (2, 1, 'Title 2',
-        '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+        'Este es un Blog de prueba, los datos son demostrativos solo para uso educativo o referencia para quien programo este blog',
         --         CURRENT_TIMESTAMP());
         {ts '2016-11-10 11:10:13.247'});
 INSERT INTO POST (post_id, user_id, title, body, create_date)
 VALUES (3, 1, 'Title 3',
-        '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+        'Este es un Blog de prueba, los datos son demostrativos solo para uso educativo o referencia para quien programo este blog',
         CURRENT_TIMESTAMP());
 INSERT INTO POST (post_id, user_id, title, body, create_date)
 VALUES (4, 1, 'Title 4',
-        '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+        'Este es un Blog de prueba, los datos son demostrativos solo para uso educativo o referencia para quien programo este blog',
         CURRENT_TIMESTAMP());
 INSERT INTO POST (post_id, user_id, title, body, create_date)
 VALUES (5, 1, 'Title 5',
-        '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+        'Este es un Blog de prueba, los datos son demostrativos solo para uso educativo o referencia para quien programo este blog',
         CURRENT_TIMESTAMP());
 INSERT INTO POST (post_id, user_id, title, body, create_date)
 VALUES (6, 1, 'Title 6',
-        '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+        'Este es un Blog de prueba, los datos son demostrativos solo para uso educativo o referencia para quien programo este blog',
         CURRENT_TIMESTAMP());
 INSERT INTO POST (post_id, user_id, title, body, create_date)
 VALUES (7, 2, 'Title 7',
-        '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+        'Este es un Blog de prueba, los datos son demostrativos solo para uso educativo o referencia para quien programo este blog',
         CURRENT_TIMESTAMP());
 INSERT INTO POST (post_id, user_id, title, body, create_date)
 VALUES (8, 2, 'Title 8',
-        '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+        'Este es un Blog de prueba, los datos son demostrativos solo para uso educativo o referencia para quien programo este blog',
         CURRENT_TIMESTAMP());
 INSERT INTO POST (post_id, user_id, title, body, create_date)
 VALUES (9, 2, 'Title 9',
-        '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+        'Este es un Blog de prueba, los datos son demostrativos solo para uso educativo o referencia para quien programo este blog',
         CURRENT_TIMESTAMP());
 INSERT INTO POST (post_id, user_id, title, body, create_date)
 VALUES (10, 2, 'Title 10',
-        '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+        'Este es un Blog de prueba, los datos son demostrativos solo para uso educativo o referencia para quien programo este blog',
         CURRENT_TIMESTAMP());
 INSERT INTO POST (post_id, user_id, title, body, create_date)
 VALUES (11, 3, 'Title 11',
-        '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+        'Este es un Blog de prueba, los datos son demostrativos solo para uso educativo o referencia para quien programo este blog',
         CURRENT_TIMESTAMP());
 INSERT INTO POST (post_id, user_id, title, body, create_date)
 VALUES (12, 3, 'Title 12',
-        '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+        'Este es un Blog de prueba, los datos son demostrativos solo para uso educativo o referencia para quien programo este blog',
         CURRENT_TIMESTAMP());
 
 -- Comments
 INSERT INTO COMMENT (post_id, user_id, body, create_date)
 VALUES (1, 1,
-        '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+        'Este es un Blog de prueba, los datos son demostrativos solo para uso educativo o referencia para quien programo este blog',
         CURRENT_TIMESTAMP());
 INSERT INTO COMMENT (post_id, user_id, body, create_date)
 VALUES (1, 2,
-        '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+        'Este es un Blog de prueba, los datos son demostrativos solo para uso educativo o referencia para quien programo este blog',
         CURRENT_TIMESTAMP());
 INSERT INTO COMMENT (post_id, user_id, body, create_date)
 VALUES (1, 3,
-        '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+        'Este es un Blog de prueba, los datos son demostrativos solo para uso educativo o referencia para quien programo este blog',
         CURRENT_TIMESTAMP());
 INSERT INTO COMMENT (post_id, user_id, body, create_date)
 VALUES (6, 1,
-        '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+        'Este es un Blog de prueba, los datos son demostrativos solo para uso educativo o referencia para quien programo este blog',
         CURRENT_TIMESTAMP());
 INSERT INTO COMMENT (post_id, user_id, body, create_date)
 VALUES (6, 2,
-        '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+        'Este es un Blog de prueba, los datos son demostrativos solo para uso educativo o referencia para quien programo este blog',
         CURRENT_TIMESTAMP());
 INSERT INTO COMMENT (post_id, user_id, body, create_date)
 VALUES (6, 3,
-        '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+        'Este es un Blog de prueba, los datos son demostrativos solo para uso educativo o referencia para quien programo este blog',
         CURRENT_TIMESTAMP());
