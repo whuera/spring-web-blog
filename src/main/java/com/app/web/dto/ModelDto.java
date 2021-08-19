5d1a5e74394e377186373bc0c63904f5ab842e35
whuera <whuera@gmail.com>
2021-08-19 17:40:01 -0500

    add Dto's

--- src/main/java/com/app/web/dto/ModelDto.java
+++ src/main/java/com/app/web/dto/ModelDto.java
@@ -1,9 +1,11 @@
 package com.app.web.dto;
 
+import com.app.web.model.Post;
 import com.app.web.model.Role;
 import com.app.web.model.User;
 
 public class ModelDto {
     private User user;
     private Role role;
+    private Post post;
 }
