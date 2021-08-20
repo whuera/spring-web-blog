ad68fb77b0127563200f3e7f38bdb4b1c8c3aa56
whuera <whuera@gmail.com>
2021-08-19 19:04:09 -0500

    add Dto's and component

--- src/main/java/com/app/web/dto/ModelDto.java
+++ src/main/java/com/app/web/dto/ModelDto.java
@@ -4,7 +4,9 @@ import com.app.web.model.Comment;
 import com.app.web.model.Post;
 import com.app.web.model.Role;
 import com.app.web.model.User;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ModelDto {
     private User user;
     private Role role;
