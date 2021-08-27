ee1fdabbca497e5e04fc1ad50d37eb38dfcd1667
Mobilpymes <whuera@gmail.com>
2021-08-27 13:45:45 -0500

    Feature/add dto model (#2)
    * add Dto's
    
    * add Dto's
    
    * add Dto's
    
    * add Dto's and component
    
    * add Dto's and component fixed
    
    add Dto's
    
    * unify commits

--- src/main/java/com/app/web/dto/ModelDto.java
+++ src/main/java/com/app/web/dto/ModelDto.java
@@ -1,7 +1,27 @@
 package com.app.web.dto;
 
+import com.app.web.model.Comment;
+import com.app.web.model.Post;
+import com.app.web.model.Role;
 import com.app.web.model.User;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ModelDto {
+
     private User user;
+    private Role role;
+    private Post post;
+    private Comment comment;
+
+    /**
+     * create constructor
+     */
+    public ModelDto() {
+    }
+    public void marshaller() {
+        ModelDto modelDto = new ModelDto();
+        modelDto.user.setName("Test1");
+        System.out.println("generate marshall");
+    }
 }
